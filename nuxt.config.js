91873d66cd9cf9cc20c71c9a8cd4d76740025b44
Jerry Tsai <purrup27@gmail.com>
2020-09-11 14:19:58 +0800

    Upload project

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,4 +64,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
+  loading: {
+    color: '#39b982',
+  },
 }
---
+++ store/events.js
@@ -0,0 +1,23 @@
+import EventService from '@/services/EventService.js'
+export const state = () => ({
+  events: [],
+  event: {},
+})
+export const mutations = {
+  SET_EVENTS(state, events) {
+    state.events = events
+  },
+  SET_EVENT(state, event) {
+    state.event = event
+  },
+}
+export const actions = {
+  async fetchEvents({ commit }) {
+    const response = await EventService.getEvents()
+    commit('SET_EVENTS', response.data)
+  },
+  async fetchEvent({ commit }, id) {
+    const response = await EventService.getEvent(id)
+    commit('SET_EVENT', response.data)
+  },
+}
